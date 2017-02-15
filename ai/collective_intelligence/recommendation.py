8c7627d80e1fa1d15fd1d773080f74e75b852b86
liubida <dada.chao.liu@gmail.com>
2017-02-15 19:26:33 +0800

    fix

---
+++ ai/collective_intelligence/recommendation.py
@@ -0,0 +1,21 @@
+
+
+def sim_distance(prefs, p1, p2):
+    si = {}
+    
+    for item in prefs[p1]:
+        if item in prefs[p2]:
+            si[item] = 1
+
+    if len(si.keys) == 0:
+        return 0 
+
+    sum()
+
+
+def main:
+    pass
+
+if __name__ == '__main__':
+    main()
+
